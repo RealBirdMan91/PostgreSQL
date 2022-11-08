0fa477704ea6494734d0447325bcaabf74829c68
RealBirdMan91 <birdman.coding.1991@gmail.com>
2022-11-08 12:40:15 +0100

    crud

---
+++ sql/02-crud-example/01-basic-table-setup.sql
@@ -0,0 +1,15 @@
+--CREATE DATABASE crud_example;
+
+
+CREATE TABLE sales (
+    id SERIAL PRIMARY KEY,
+    date_created DATE DEFAULT CURRENT_DATE,
+    date_fulfilled DATE,
+    customer_name VARCHAR(200) NOT NULL,
+    customer_email VARCHAR(200) NOT NULL UNIQUE,
+    product_name VARCHAR(200) NOT NULL,
+    price NUMERIC(10,2) NOT NULL CHECK (price >= 0),
+    is_recurring BOOLEAN DEFAULT FALSE,
+    is_disputed BOOLEAN DEFAULT FALSE
+)
+
