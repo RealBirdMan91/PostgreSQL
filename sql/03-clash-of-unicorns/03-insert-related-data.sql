3d4e151a6737a24ac458b64e3a572ecc1a8791c7
RealBirdMan91 <birdman.coding.1991@gmail.com>
2022-11-23 19:20:19 +0100

    Update 03-insert-related-data.sql

--- sql/03-clash-of-unicorns/03-insert-related-data.sql
+++ sql/03-clash-of-unicorns/03-insert-related-data.sql
@@ -1,8 +1,18 @@
 --INSERT INTO locations (area_name, lat, lng, area_type)
 --VALUES ('Mushroom Castel', 233.55, 55.77, 'Mushroom forest');
 
-INSERT INTO locations (area_name, lat, lng, area_type)
-VALUES ('Candy Cave', 563.85, 905.77, 'sugar mountains'),
-('Sugar swamp', 567.99, 306.77, 'cotton candy clouds'),
-('Pink cotton candy fort', 577.99, 336.77, 'cotton candy clouds'),
-('rough cave camp', 573.99, 866.47, 'gummy worms caves');
+--INSERT INTO locations (area_name, lat, lng, area_type)
+--VALUES ('Candy Cave', 563.85, 905.77, 'sugar mountains'),
+--('Sugar swamp', 567.99, 306.77, 'cotton candy clouds'),
+--('Pink cotton candy fort', 577.99, 336.77, 'cotton candy clouds'),
+--('rough cave camp', 573.99, 866.47, 'gummy worms caves');
+
+
+
+INSERT INTO unicorns (unicorn_name, unicorn_status, unicorn_personality, location_id)
+VALUES ('Pummelcorn', 'magical_one', 'cuddly', 4),
+('Angrycorn', 'warrior', 'grumpy', 5),
+('Strangecorn', 'magical_one', 'hyperactive', 4),
+('Lazycorn', 'worker', 'lazy', 1),
+('Cookiecorn', 'worker', 'greedy', 1),
+('Sleepycorn', 'worker', 'funny', 3);
