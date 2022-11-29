57ebef0c6a0920e9ff8f2465833f9efcdcd3e635
RealBirdMan91 <birdman.coding.1991@gmail.com>
2022-11-29 12:47:33 +0100

    Update 01-relationships.sql

--- sql/04-office/01-relationships.sql
+++ sql/04-office/01-relationships.sql
@@ -1,8 +1,14 @@
-CREATE TABLE employees(
+CREATE TABLE intranet_accounts (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(200) REFERENCES employees (email) ON ,
+    password VARCHAR(200) NOT NULL
+);
+
+CREATE TABLE employees (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(200) NOT NULL,
     last_name VARCHAR(200) NOT NULL,
     birthdate DATE NOT NULL,
-    email VARCHAR(200) NOT NULL UNIQUE,
+    email VARCHAR(200) UNIQUE NOT NULL,
     created_at DATE DATE DEFAULT CURRENT_TIMESTAMP,
-)
+);
