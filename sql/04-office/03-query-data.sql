e5dea5f86f24136b63436c09daf8b39461e33fde
RealBirdMan91 <birdman.coding.1991@gmail.com>
2022-11-30 22:59:52 +0100

    Update 03-query-data.sql

--- sql/04-office/03-query-data.sql
+++ sql/04-office/03-query-data.sql
@@ -2,7 +2,12 @@
 --LEFT JOIN projects_employees AS pe ON pe.employee_id = e.id
 --LEFT JOIN projects AS p ON pe.project_id = p.id;
 
-SELECT e.id AS employee_id, e.first_name, e.last_name, t.name AS team_name
-FROM employees as e
-INNER JOIN teams as t ON e.team_id = t.id
-WHERE t.id = 2;
+--SELECT e.id AS employee_id, e.first_name, e.last_name, t.name AS team_name
+--FROM employees as e
+--INNER JOIN teams as t ON e.team_id = t.id
+--WHERE t.id = 2;
+
+SELECT e.id AS employee_id, e.first_name, e.last_name, t.name AS team_name, t.id AS team_id, t.building_id, b.name AS building_name
+FROM employees AS e
+INNER JOIN teams AS t ON e.team_id = t.id
+INNER JOIN company_buildings AS b ON t.building_id = b.id;
