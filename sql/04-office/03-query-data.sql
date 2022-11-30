8c5315a0b4c5625a889c4bc3134990a09c011d22
RealBirdMan91 <birdman.coding.1991@gmail.com>
2022-11-30 23:04:34 +0100

    Update 03-query-data.sql

--- sql/04-office/03-query-data.sql
+++ sql/04-office/03-query-data.sql
@@ -7,7 +7,9 @@
 --INNER JOIN teams as t ON e.team_id = t.id
 --WHERE t.id = 2;
 
-SELECT e.id AS employee_id, e.first_name, e.last_name, t.name AS team_name, t.id AS team_id, t.building_id, b.name AS building_name
+SELECT e.id AS employee_id, e.first_name,e.birthdate, e.last_name, t.name AS team_name, t.id AS team_id, t.building_id, b.name AS building_name
 FROM employees AS e
 INNER JOIN teams AS t ON e.team_id = t.id
-INNER JOIN company_buildings AS b ON t.building_id = b.id;
+INNER JOIN company_buildings AS b ON t.building_id = b.id
+WHERE b.name = 'Darkroom'
+ORDER BY e.birthdate;
