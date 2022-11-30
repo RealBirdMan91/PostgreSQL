0eaeb59c12813dd35e0eebfc376f5d686a157046
RealBirdMan91 <birdman.coding.1991@gmail.com>
2022-11-30 22:47:59 +0100

    Update 03-query-data.sql

--- sql/04-office/03-query-data.sql
+++ sql/04-office/03-query-data.sql
@@ -1,3 +1,8 @@
-SELECT e.first_name, e.last_name, e.id AS employee_id, p.title, p.id AS project_id FROM employees AS e
-LEFT JOIN projects_employees AS pe ON pe.employee_id = e.id
-LEFT JOIN projects AS p ON pe.project_id = p.id;
+--SELECT e.first_name, e.last_name, e.id AS employee_id, p.title, p.id AS project_id FROM employees AS e
+--LEFT JOIN projects_employees AS pe ON pe.employee_id = e.id
+--LEFT JOIN projects AS p ON pe.project_id = p.id;
+
+SELECT e.id AS employee_id, e.first_name, e.last_name, t.name AS team_name
+FROM employees as e
+INNER JOIN teams as t ON e.team_id = t.id
+WHERE t.id = 2;
